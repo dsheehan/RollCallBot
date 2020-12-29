3ad2712e88d3d4e10ce817336b1604243760e6d4
dsheehan <dsheehan@gmail.com>
2020-12-29 00:24:08 -0500

    Allow "Roll Call" without args. Rename class.

--- Commands.cs
+++ Commands.cs
@@ -10,12 +10,12 @@ using Microsoft.Extensions.Logging;
 namespace RollCallBot
 {
     // Create a module with no prefix
-    public class InfoModule : ModuleBase<SocketCommandContext>
+    public class Commands : ModuleBase<SocketCommandContext>
     {
         private readonly MessageHandler _messageHandler;
-        private readonly ILogger<InfoModule> _logger;
+        private readonly ILogger<Commands> _logger;
 
-        public InfoModule(MessageHandler messageHandler, ILogger<InfoModule> logger)
+        public Commands(MessageHandler messageHandler, ILogger<Commands> logger)
         {
             _messageHandler = messageHandler;
             _logger = logger;
@@ -32,6 +32,20 @@ namespace RollCallBot
             await Context.Message.AddReactionAsync(emoji);
             // var message = new Message(description);
             var message = _messageHandler.AddNewMessage(description);
+            await message.Send(Context.Channel);
+        }
+        
+        [Command("Roll Call", RunMode = RunMode.Async)]
+        [Alias("RollCall")]
+        [Summary("Start a roll call")]
+        public async Task ReactAsync2()
+        {
+            _logger.LogInformation($"New Roll Call");
+            var emoji = new Emoji("\uD83D\uDC4C");   // equivalent to "👌"
+            
+            await Context.Message.AddReactionAsync(emoji);
+            // var message = new Message(description);
+            var message = _messageHandler.AddNewMessage(null);
             await message.Send(Context.Channel);
         }
     }
